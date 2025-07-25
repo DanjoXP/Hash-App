b2c2096c7db11707b879dcfdf8a7c32dcc9e1fd7
DanjoXP <191726617+DanjoXP@users.noreply.github.com.>
2025-07-25 16:06:11 +0100

    Second Commit

--- gui_handler.py
+++ gui_handler.py
@@ -4,15 +4,22 @@ from tkinter import ttk
 
 class App:
     
-    root,label,frame,hash_button = None,None,None,None
+    root,label,frame,hash_button,hash_type = None,None,None,None,None
     
+    hash_types = ('sha1','sha224','sha256','sha384','sha512'
+                 ,'sha_224','sha_256','sha3_384','sha3_512',
+                 'shake_128','shake_256','blake2b','blake2s','md5')
+    
+    # populate window intentionally mangled to make it private and to discourage calls from outside of class.
+    @staticmethod
     def __populate_window():
         App.label = Label(App.root, text="Hello World")
         App.label.pack()
         App.hash_button = Button(App.root,text="HASH",width=50)
         App.hash_button.pack()
-                
-        
+        App.hash_type = ttk.Combobox(App.root, values= App.hash_types)
+        App.hash_type.state(["readonly"])
+        App.hash_type.pack()       
         
     @staticmethod
     def create_window(X,Y,TITLE):
